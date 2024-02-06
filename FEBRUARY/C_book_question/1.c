c2f81293f1a46f5ebb1b24ecb5402b10dba25dc7
Gourav anand <90522738+venomusblood568@users.noreply.github.com>
2024-02-06 22:09:10 +0530

    Create 1.c

---
+++ FEBRUARY/C_book_question/1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int a , b , area, perimeter;
+    a = 3;
+    b = 8;
+    area = a * b;
+    perimeter = 2 * (a + b);
+    printf("%d \n", area);
+    printf("%d \n", perimeter);
+    return 0;
+}
