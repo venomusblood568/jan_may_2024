92d860f121a8cda967f711f4182a506ed068afc6
Gourav anand <90522738+venomusblood568@users.noreply.github.com>
2024-02-06 22:19:35 +0530

    Update 1.c

--- FEBRUARY/C_book_question/1.c
+++ FEBRUARY/C_book_question/1.c
@@ -10,3 +10,18 @@ int main() {
     printf("%d \n", perimeter);
     return 0;
 }
+
+/*
+//user input
+#include <stdio.h>
+
+int main() {
+    int a , b , area, perimeter;
+    scanf("%d %d", &a,&b);
+    area = a * b;
+    perimeter = 2 * (a + b);
+    printf("%d \n", area);
+    printf("%d \n", perimeter);
+    return 0;
+}
+*/
